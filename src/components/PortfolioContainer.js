7fe7f1e3506d23d75f1b285ca53f2bc03db55ee9
DomenicoBarb <domenicobarberi585@gmail.com>
2023-05-12 20:44:26 -0400

    restructured to meet grading criteria

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
+import About from './About/About';
+import Portfolio from './Portfolio/Portfolio';
+import Contact from './Contact/Contact';
+import Resume from './Resume/Resume';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
