cd6e3773884cdb11e54130250f9ed7897ed548b6
DomenicoBarb <domenicobarberi585@gmail.com>
2023-05-14 00:04:08 -0400

    src/App.js & index.js updated, app.js incorperating renderTab, index.js incorperated strictmode to check for issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Resume from "./components/Resume/Resume";
 
-const App = () => <PortfolioContainer />;
+function App() {
+	const [currentTab, setCurrentTab] = useState("about");
+
+	// This function checks to see which tab is selected and then generates the appropriate tab.
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div className="master-div">
+			<div className="mobile-header">
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+			<div>
+				<Footer></Footer>
+			</div>
+		</div>
+	);
+}
 
 export default App;
