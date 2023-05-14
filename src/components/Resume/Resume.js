dc131f8c530d7227fdf226b8e85dc55a915270e7
DomenicoBarb <domenicobarberi585@gmail.com>
2023-05-14 00:28:11 -0400

    Imported react icons for download button styling, added downloadable resume in pdf format, added raw text web dev experience

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React from "react";
+import { BsCloudDownload } from "react-icons/bs";
 
-export default function Resume() {
-  return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
-  );
+function Resume() {
+	return (
+		<section>
+			<div className="center">
+				<h1 className="page-header">My Resume:</h1>
+			</div>
+			<div className="bottom-spacing">
+				<a href={require("../../assets/resume/DomenicoBarberi2023.pdf")} download>
+				<h4 className='resume-h4'><BsCloudDownload className="BsCloudDownload" /> Download Resume <BsCloudDownload className="BsCloudDownload" />
+				</h4>
+				</a>
+			</div>
+			<div className="resume-formatting">
+				<h5>Front-End Experience:</h5>
+				<ol>
+					<li>HTML5</li>
+					<li>CSS3</li>
+					<li>JavaScript</li>
+					<li>jQuery</li>
+					<li>Bootstrap</li>
+					<li>React</li>
+					<li>version control/Git</li>
+					<li>responsive design</li>
+				</ol>
+				<br></br>
+				<h5>Back-End Experience:</h5>
+				<ol>
+					<li>APIs</li>
+					<li>NodeJS</li>
+					<li>Express</li>
+					<li>Model View Controller (MVC)</li>
+					<li>REST</li>
+					<li>Progressive Web Applications (PWA)</li>
+				</ol>
+				<br></br>
+				<h5>Dev Tool Experience:</h5>
+				<ol>
+					<li>Git</li>
+					<li>npm</li>
+					<li>Jest</li>
+					<li>Webpack</li>
+				</ol>
+				<br></br>
+				<h5>Database Technologies Experience:</h5>
+				<ol>
+					<li>MySQL</li>
+					<li>Sequelize</li>
+					<li>NoSQL</li>
+					<li>MongoDB</li>
+					<li>Mongoose</li>
+				</ol>
+			</div>
+		</section>
+	);
 }
+
+export default Resume;
