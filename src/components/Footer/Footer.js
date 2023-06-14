e8c56071cb430d0df5dddd74734076743508c996
DomenicoBarb <domenicobarberi585@gmail.com>
2023-06-13 22:35:38 -0400

    added footer placeholder for linkedIn

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,35 @@
 import React from "react";
 
 function Footer() {
-	return (
-		<footer className="footer">
-			<div>
-				<a
-					href="https://github.com/DomenicoBarb"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src={require("../../images/github-logo.png")}
-						alt="Github"
-						className="logo"
-						title="My GitHub profile"
-					></img>
-				</a>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <a
+        href="https://github.com/DomenicoBarb"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={require("../../images/github-logo.png")}
+          alt="Github"
+          className="logo"
+          title="My GitHub profile"
+        />
+      </a>
+	  {/* placeholder for linkedin */}
+      <a
+        href="https://github.com/DomenicoBarb"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={require("../../images/github-logo.png")}
+          alt="Github"
+          className="logo"
+          title="My GitHub profile"
+        />
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
